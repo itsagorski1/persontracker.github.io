cb3c81c6fef2f348c84f5b9c5c89336963f41d66
itsagorski1 <jonahisaacgorski73@gmail.com>
2026-04-26 15:36:16 -0700

    do this stuff

--- src/main/java/com/jonah/code/java/random/persontracker/person/fileeditor/FileEditor.java
+++ src/main/java/com/jonah/code/java/random/persontracker/person/fileeditor/FileEditor.java
@@ -1,15 +1,36 @@
 package com.jonah.code.java.random.persontracker.person.fileeditor;
 
-import java.io.FileWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class FileEditor {
-    public FileEditor(String fileName,String type,String content) {
-        try (FileWriter writer = new FileWriter(fileName)) {
-            writer.write(content);
-            System.out.println(content+" written to " + fileName);
+    public FileEditor(String fileName, String type, String[] ppl, String content) {
+        String pythonCommand = "python3"; // or "python" depending on your system
+        String pythonScript = "'C:\\Users\\jonah\\code\\java\\random\\persontracker\\Epython\\FileEditor.py'";
+        ProcessBuilder processBuilder = new ProcessBuilder(pythonCommand, pythonScript, fileName, type, Arrays.toString(ppl), content);
+        try {
+            Process process = processBuilder.start();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println("Python Output: " + line);
+                }
+            }
+            try (BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
+                String line;
+                while ((line = errorReader.readLine()) != null) {
+                    System.err.println("Python Error: " + line);
+                }
+            }
+            int exitCode = process.waitFor();
+            System.out.println("Python process exited with code: " + exitCode);
         } catch (IOException e) {
-            System.err.println("Error writing to file: "e.getMessage());
+            System.err.println("Error executing Python command: " + e.getMessage());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Python process was interrupted.");
         }
     }
 }
--- src/main/java/com/jonah/code/java/random/persontracker/person/relationship/divorce/Divorce.java
+++ src/main/java/com/jonah/code/java/random/persontracker/person/relationship/divorce/Divorce.java
@@ -18,7 +18,7 @@ public class Divorce {
         person1.person.put("yr_divorced",sc.nextLine().trim());
         person2.person.put("yr_divorced",person1.person.get("yr_divorced"));
         System.out.println("Divorce finalized.");
-        FileEditor fe = new FileEditor("people.txt","upd","div p1 "+person1.person.get("name")+" div p2 "+person2.person.get("name")+" yr div "+person1.person.get("yr_divorced"));
-        );
+        String[] toPass = {person1.person.get("name"), person2.person.get("name")};
+        FileEditor fe = new FileEditor("people.txt","upd", toPass,"div p1 "+person1.person.get("name")+" div p2 "+person2.person.get("name")+" yr div "+person1.person.get("yr_divorced"));
     }
 }
